92a2a05de17ac9d109b2448fef215f80a537f2bb
Matheus Ferreira <matheus.ferreira@VALEMOBI.CORP@VMNOTE-136.valemobi.corp>
2021-05-01 02:19:08 -0300

    feat: saldo com servico

--- src/pages/Painel/index.jsx
+++ src/pages/Painel/index.jsx
@@ -8,22 +8,29 @@ import api from '../../services/api'
 
 function Painel() {
   const {dados} = React.useContext(UserContext);
-  const {saldo, setSaldo} = React.useState(null);
+  const [saldo, setSaldo] = React.useState(null);
+  const [info, setInfo] = React.useState(null);
 
-  // React.useEffect(() => {
-  //   async function fetchData() {
-  //     if (dados) {
-  //       const response = await api.get(`/economigos/u/${dados.id}`);
-  //       setData(await );
-  //     }
-  //   }
-  //   fetchData();
-  // }, [dados]);
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  React.useEffect(() => {
+    if (info != null) {
+      setSaldo(info.valorAtual)
+    }
+  }, [info])
   
+  async function fetchData() {
+    if (dados) {
+      const response = await api.get(`/economigos/usuarios/${dados.usuario.id}`);
+      setInfo(await response.data);
+    }
+  }
   
   return (
     <>
-        <SaldoTotal/>
+        <SaldoTotal saldo={saldo != null && saldo}/>
         <GastosPorCategoria/>
         <BalancoMensal/>
     </>
