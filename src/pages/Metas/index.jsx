8b3de884a99355ac726a24bac62054338ca8d023
Aluno <gabriel.pinheiro@bandtec.com.br>
2021-05-08 02:37:36 -0300

    feat: tela de metas && card de metas ambos v1

--- src/pages/Metas/index.jsx
+++ src/pages/Metas/index.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import * as S from './style'
+import api from "../../services/api";
+import { UserContext } from '../../hooks/UserContext'
+import BotaoMenuMetas from "../../components/BotaoMenuMetas/BotaoMenuMetas";
+import BotaoAdicionar from "../../assets/botao-adicionar.svg";
+import Aviao from "../../assets/aviao.svg";
+import CardMetas from "../../components/CardMetas/CardMetas";
 
 export default function Metas() {
+
+    const { dados } = React.useContext(UserContext);
+    const [metas, setMetas] = React.useState([]);
+
+    React.useEffect(() => {
+        fetchMetas();
+    }, [dados]);
+
+    async function fetchMetas() {
+        if (dados) {
+            const response = await api.get(`/economigos/usuarios/${dados.usuario.id}`);
+            setMetas(await response.data.metaDtos);
+        }
+    }
+
+    console.log(metas)
+
     return (
-        <div>
-            tela metas
-        </div>
+        <>
+            <S.NavMetas>
+                <BotaoMenuMetas nome="Metas Ativas" active />
+                <BotaoMenuMetas nome="Metas Inativas" />
+                <BotaoMenuMetas nome="Metas Finalizadas" />
+            </S.NavMetas>
+            <S.AdicionarMeta>
+                <img src={BotaoAdicionar} alt="" />
+            </S.AdicionarMeta>
+            <S.Cards style={metas.length > 2 ? {overflowY: "scroll"} : {overflowY: "hidden"}}>
+                {
+                    metas.map(itemMeta => (
+                        <CardMetas nome={itemMeta.nome} urlImage={Aviao} valorFinal={itemMeta.valorFinal} valorInicial={itemMeta.valorInicial + 1000} />
+                    ))
+                }
+            </S.Cards>
+        </>
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: "http://localhost:8080"});
+const api = axios.create({baseURL: "http://ip172-18-0-52-c2av1u5mrepg008v8ndg-8080.direct.labs.play-with-docker.com/"});
 
 
 export default api;
