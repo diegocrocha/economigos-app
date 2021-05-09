3cc5769920ce7361a6ac3ece4e174a4c4bd6c9cf
Aluno <gabriel.pinheiro@bandtec.com.br>
2021-05-09 20:41:32 -0300

    feat: adicionando variavel de ambiente na aplicacao

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: "http://ip172-18-0-28-c2c5m8fqf8u0009kurpg-8080.direct.labs.play-with-docker.com/"});
+const api = axios.create({baseURL: process.env.REACT_APP_API_URL});
 
 export default api;
