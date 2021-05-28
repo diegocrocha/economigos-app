bfe6dab684e74f30ca1e5631d359a40fade4b46a
VMarkz <vitor.portela@bandtec.com.br>
2021-05-28 17:31:26 -0300

    Alterar baseURL para container Docker

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: process.env.REACT_APP_API_URL});
+// const api = axios.create({baseURL: process.env.REACT_APP_API_URL});
+const api = axios.create({baseURL: "http://ip172-18-0-16-c2oki6fqf8u000arr8bg-8080.direct.labs.play-with-docker.com/"})
 
 export default api;
