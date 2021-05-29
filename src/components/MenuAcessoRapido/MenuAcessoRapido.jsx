6a047855ecf72d2a12fc097ac1a0c8b7660ff49b
VMarkz <vitor.portela@bandtec.com.br>
2021-05-29 16:46:06 -0300

    adicionando modal extrato ao menuAcessoRapido

--- src/components/MenuAcessoRapido/MenuAcessoRapido.jsx
+++ src/components/MenuAcessoRapido/MenuAcessoRapido.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import * as S from './style'
 import OpcoesAcessoRapido from "../../components/OpcoesAcessoRapido/OpcoesAcessoRapido";
 import ModalContabil from '../ModalContabil/ModalContabil';
+import ModalExtrato from '../ModalExtrato/ModalExtrato';
 
 export default function MenuAcessoRapido() {
   const [modalR, setModalR] = React.useState(false);
   const [modalG, setModalG] = React.useState(false);
-
-
+  const [modalE, setModalE] = React.useState(false);
 
     return (
         <S.MenuAcessoRapido>
@@ -24,7 +24,11 @@ export default function MenuAcessoRapido() {
               modal={modalG}
               setModal={setModalG} />
             <OpcoesAcessoRapido nome={"Telegram"} cor={" #0088cc"} />
-            <OpcoesAcessoRapido click={() => setModalG(true)} nome={"Extratos"} cor={"#312F2F"} />
+            <OpcoesAcessoRapido click={() => setModalE(true)} nome={"Extratos"} cor={"#312F2F"} />
+            <ModalExtrato
+              color="#312F2F"
+              modal={modalE}
+              setModal={setModalE} />
         </S.MenuAcessoRapido>
     )
 }
