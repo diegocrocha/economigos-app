fd38310a6daa20d523c9eb8b0e15127028e657d4
Aluno <gabriel.pinheiro@bandtec.com.br>
2021-06-04 18:23:33 -0300

    feat: tela painel finalizada com sucesso

--- src/components/GastosPorCategoria/GastosPorCategoria.jsx
+++ src/components/GastosPorCategoria/GastosPorCategoria.jsx
@@ -9,15 +9,26 @@ export default function GastosPorCategoria({ dataCategorias, preenchido }) {
     const [maiorCategoria, setMaiorCategoria] = React.useState("")
     const [meioCategoria, setMeioCategoria] = React.useState("")
     const [menorCategoria, setMenorCategoria] = React.useState("")
+    const [somas, setSomas] = React.useState(null)
 
     React.useEffect(() => {
         if (preenchido) {
-            setMaiorCategoria(dataCategorias[0])
-            setMeioCategoria(dataCategorias[1])
-            setMenorCategoria(dataCategorias[2])
+            setMaiorCategoria(dataCategorias[0]);
+            setMeioCategoria(dataCategorias[1]);
+            setMenorCategoria(dataCategorias[2]);
+            soma(dataCategorias);
         }
 
     }, [dataCategorias])
+
+    function soma(dataCategorias) {
+        let total = 0;
+        for (let i = 0; i < dataCategorias.length; i++) {
+            total += dataCategorias[i].soma;
+        }
+        console.log(total)
+        setSomas(total)
+    }
 
     return (
         <S.GastosPorCategoria>
@@ -26,17 +37,21 @@ export default function GastosPorCategoria({ dataCategorias, preenchido }) {
                 <span>Gastos</span>
             </div>
 
-            {preenchido ?
+            {preenchido && somas != 0 ?
                 <>
                     <div className="listaDeCategorias">
                         <ul>
-                            <ItemListaCategoria cor={"#A23232"} nomao={maiorCategoria.nome}></ItemListaCategoria>
-                            <ItemListaCategoria cor={"#4d1717"} nomao={meioCategoria.nome}></ItemListaCategoria>
-                            <ItemListaCategoria cor={"#270c0c"} nomao={menorCategoria.nome}></ItemListaCategoria>
-                            <ItemListaCategoria cor={"#000"} nomao={"Outros"}></ItemListaCategoria>
+                            <ItemListaCategoria cor={"#A23232"} nome={maiorCategoria.nome}></ItemListaCategoria>
+                            <ItemListaCategoria cor={"#4d1717"} nome={meioCategoria.nome}></ItemListaCategoria>
+                            <ItemListaCategoria cor={"#270c0c"} nome={menorCategoria.nome}></ItemListaCategoria>
+                            <ItemListaCategoria cor={"#000"} nome={"Outros"}></ItemListaCategoria>
                         </ul>
                     </div>
                     <div className="graficoDeCategorias">
+                        <div className="totalGastos">
+                            <span>Gastos</span>
+                            <span><span>R$</span>{Number(somas).toLocaleString('pt-br', { minimumFractionDigits: 2 })}</span>
+                        </div>
                         <GroupPieChart maior={maiorCategoria} meio={meioCategoria} menor={menorCategoria} />
                     </div>
                 </>
