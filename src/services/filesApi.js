a1f29497400dc115c43bbadb2e93607dc0900d41
VMarkz <vitor.portela@bandtec.com.br>
2021-06-08 13:17:02 -0300

    Descomentando variavel de ambiente

--- src/services/filesApi.js
+++ src/services/filesApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// const filesApi = axios.create({baseURL: process.env.REACT_APP_FILES_API_URL});
-const filesApi = axios.create({baseURL: "http://ip172-18-0-71-c2ru2v7njsv000dqnueg-8081.direct.labs.play-with-docker.com/"})
+const filesApi = axios.create({baseURL: process.env.REACT_APP_FILES_API_URL});
+// const filesApi = axios.create({baseURL: "http://localhost:8081/"})
 
 export default filesApi;
